dfbc12a6e83f342406b4476fbc8d77fb49f26642
doloreses <popokaja9@gmail.com>
2022-10-06 11:15:14 +0200

    aplikacja

--- app/src/main/java/com/example/nowyprojekt/MainActivity.java
+++ app/src/main/java/com/example/nowyprojekt/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.nowyprojekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public int klikniete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void zlaOdpowiedz(View view) {
+        Toast.makeText(MainActivity.this,R.string.zla_odp,Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    public void dobraOdpowiedz(View view) {
+        view.setVisibility(View.INVISIBLE);
+        klikniete++;
+        if(klikniete==5){
+            TextView pytanieTextView = findViewById(R.id.pytanieTextView);
+        }
     }
 }
